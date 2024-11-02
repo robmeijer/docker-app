03e2d80a4767f6ad405d8435442aec24c49c53b7
Rob Meijer <1022844+robmeijer@users.noreply.github.com>
2024-11-02 18:43:49 +0000

    WIP

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -8,7 +8,6 @@ fi
 
 if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
   if [ "$APP_ENV" = 'dev' ]; then
-    ln -sf "$PHP_INI_DIR/php.ini-development" "$PHP_INI_DIR/php.ini"
     composer install --prefer-dist --no-progress --no-interaction
     chmod -R 777 var
   fi
